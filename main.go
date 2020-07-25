f3ca24bf5a0d654bd5152160bf1745895ae4d9a2
Adam Derewecki <derewecki@gmail.com>
2020-07-24 21:24:12 -0700

    Add allowed methods

--- main.go
+++ main.go
@@ -144,6 +144,7 @@ func main() {
 	router.OPTIONS("/v1/dbtest", func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS")
 		c.JSON(http.StatusOK, struct{}{})
 	})
 
