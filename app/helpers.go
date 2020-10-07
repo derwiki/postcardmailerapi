09ac5298a9c289ba376635b025f594c5d1d572af
Adam Derewecki <derewecki@gmail.com>
2020-10-07 15:32:23 -0700

    CORS: echo HttpOrigin back for allow-origin; allow-credentials: true

--- app/helpers.go
+++ app/helpers.go
@@ -1,11 +1,18 @@
 package helpers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetCorsHeaders(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	var origin = c.GetHeader("Origin")
+	fmt.Println("origin", origin)
+	c.Header("Access-Control-Allow-Origin", origin)
 	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
 	c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Content-Type", `application/json`)
 	c.Header("Accept", `application/json`)
 }
